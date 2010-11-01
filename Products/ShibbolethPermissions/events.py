72c5f7bb8d7601d3528d3f52044db77094d581e1
tom_gross <tom_gross@localhost>
2010-11-01 12:56:37 +0000

    don't fail on subscriber, if plugin is not installed
    svn path=/Products.ShibbolethPermissions/trunk/; revision=225815

--- Products/ShibbolethPermissions/events.py
+++ Products/ShibbolethPermissions/events.py
@@ -1,7 +1,15 @@
+import logging
+
+log = logging.getLogger('ShibbolethPermissions')
+
 def refreshlocalroles(event):
     user = event.principal
     pas = user._getPAS()
 
+    if 'ShibbolethPermissions' not in pas.keys():
+        log.warn(("ShibbolethPermissions plugin not found! "
+                  "Cannot refresh local roles."))
+        return
     shibtool = pas['ShibbolethPermissions']
     shibtool.refreshlocalroles(user)
 
